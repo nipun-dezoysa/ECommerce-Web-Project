8e6506a6141d71d96036db3c8c8207a3a7997a65
Dilshan Sanjeewa Deshapriya <dilshansanjeewadeshappriya@gmail.com>
2024-04-04 21:09:41 +0530

    admin login backend and database

--- src/java/mainPackage/addProduct.java
+++ src/java/mainPackage/addProduct.java
@@ -52,3 +52,4 @@ public class addProduct extends HttpServlet {
         }
     }
 }
+
--- src/java/mainPackage/adminSignIn.java
+++ src/java/mainPackage/adminSignIn.java
@@ -6,6 +6,7 @@
 package mainPackage;
 
 import java.io.IOException;
+import java.io.PrintWriter;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -23,16 +24,31 @@ public class adminSignIn extends HttpServlet {
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
+        PrintWriter out = response.getWriter();
+        
         
         HttpSession session = request.getSession(true);
         String email = request.getParameter("email");
         String password = request.getParameter("password");
+        DatabaseLogIn db3 = new DatabaseLogIn();
         
         if(email!=null && password!=null){
             //database functions
-            session.setAttribute("admin", email);
+            db3.adminSignIn(email, password);
+            if(db3.adminislogin()){
+                session.setAttribute("admin", email);
+                out.println("admin "+ email +"succesessfully loged in ");
+                response.sendRedirect("http://localhost:8080/ECommerce_Web_Project/admin/");
+                
+            
+            
+            }
+            else{
+                response.sendRedirect("http://localhost:8080/ECommerce_Web_Project/admin/index.jsp");
+            }
+            
         }
-        response.sendRedirect("http://localhost:8080/ECommerce_Web_Project/admin/");
+        
         
     }
 
