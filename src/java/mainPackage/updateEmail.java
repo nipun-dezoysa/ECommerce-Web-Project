8601d1bb2074fd4fa9cbf9a91c285a19d106bd63
Dilshan Sanjeewa Deshapriya <dilshansanjeewadeshappriya@gmail.com>
2024-04-18 14:28:41 +0530

    make a update user details  update user email and password and validation backend

---
+++ src/java/mainPackage/updateEmail.java
@@ -0,0 +1,60 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package mainPackage;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import models.User;
+import models.userUpdate;
+
+/**
+ *
+ * @author sanjeewa
+ */
+@WebServlet(name = "updateEmail", urlPatterns = {"/updateEmail"})
+public class updateEmail extends HttpServlet {
+
+    
+
+   
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        HttpSession session = request.getSession();
+        User user = (User)session.getAttribute("user");
+        int id = user.getId();
+        String email=request.getParameter("email");
+        String passwd=request.getParameter("password");
+        userUpdate uup = new userUpdate();
+        int status = uup.updateEmail(email, passwd, id);
+        response.sendRedirect("signin.jsp");
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
